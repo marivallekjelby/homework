6f4c3908bc98a752985d12dd563c68f6ec752024
marivallekjelby <103957924+marivallekjelby@users.noreply.github.com>
2022-06-16 15:32:17 +0200

    Add files via upload

---
+++ exercises/5. File input and output/main(complex).cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+using static System.Math;
+using static cmath;
+
+public class main
+{
+	static void Main()
+	{
+
+		//Calculations 
+		complex minus_one = -1;
+		WriteLine($"sqrt(-1) = {sqrt(minus_one)}");
+		WriteLine($"sqrt(i) = {sqrt(I)}");
+		WriteLine($"exp(i) = {exp(I)}");
+		WriteLine($"exp(i*pi) = {exp(I * PI)}");
+		WriteLine($"i^i = {cmath.pow(I, I)}");
+		WriteLine($"ln(i) = {log(I)}");
+		WriteLine($"sin(i*pi) = {sin(I * PI)}");
+		WriteLine($"sinh(i) = {sinh(I)}");
+		WriteLine($"cosh(i) = {cosh(I)}");
+	}
+}
