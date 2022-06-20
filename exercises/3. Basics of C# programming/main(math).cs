527a10fde171de1a45a05e97ac3f0a71baa328e6
marivallekjelby <103957924+marivallekjelby@users.noreply.github.com>
2022-06-21 01:11:42 +0200

    Add files via upload

---
+++ exercises/3. Basics of C# programming/main(math).cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using static System.Math;
+
+class math
+{
+	static void Main()
+	{
+		double sqrt2 = Sqrt(2.0);
+		Write($"sqrt(2) = {sqrt2}\n");
+		Write($"sqrt(2)*sqrt(2) = {sqrt2 * sqrt2} (should be equal 2)\n");
+
+		double epi = Exp(PI);
+		Write($"exp(pi) = {epi}\n");
+		Write($"ln(exp(pi) = {Log(epi)} (should be equal pi)\n");
+
+		double pie = Pow(PI, E);
+		Write($"pi^e = {pie}\n");
+		Write($"pow(pi^e, 1/e) = {Pow(pie, 1 / E)} (should be equal to pi)\n");
+	}
+}
